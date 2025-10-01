23e6b6c28651bf87de711da2421591be2614a52e
MichaelCache <faith_gentoo@163.com>
2025-10-01 21:15:16 +0800

    <fix>fix float grammar error

--- src/lib/parser/base_grammar/float_num.h
+++ src/lib/parser/base_grammar/float_num.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "int_num.h"
-#include "pegtl.hpp"
+#include "tao/pegtl.hpp"
 
 namespace CAEParser {
 namespace peg = tao::pegtl;
@@ -15,15 +15,14 @@ struct num_point
 
 // match "1.", ".1","1.1" like but not endwith e/E as float
 struct real_num
-    : peg::seq<peg::opt<peg::one<'-', '+'>, peg::sor<point_num, num_point>,
-                        peg::not_at<peg::one<'e', 'E'>>>> {};
+    : peg::seq<peg::opt<peg::one<'-', '+'>>, peg::sor<point_num, num_point>,
+               peg::not_at<peg::one<'e', 'E'>>> {};
 
 // sci num
 struct exp : peg::seq<peg::one<'e', 'E'>, sign_int_num> {};
 struct sci_real_num
-    : peg::seq<
-          peg::opt<peg::one<'-', '+'>,
-                   peg::sor<peg::seq<peg::plus<peg::digit>, exp>,
-                            peg::seq<peg::sor<point_num, num_point>, exp>>>> {};
+    : peg::seq<peg::opt<peg::one<'-', '+'>>,
+               peg::sor<peg::seq<peg::plus<peg::digit>, exp>,
+                        peg::seq<peg::sor<point_num, num_point>, exp>>> {};
 struct float_num : peg::sor<real_num, sci_real_num> {};
 }  // namespace CAEParser
--- test/base_grammar_test.cpp
+++ test/base_grammar_test.cpp
@@ -1,13 +1,38 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #define DOCTEST_CONFIG_NO_EXCEPTIONS
 #include "doctest.h"
+#include "parser/base_grammar/float_num.h"
 #include "parser/base_grammar/int_num.h"
 #include "tao/pegtl/contrib/trace.hpp"
 #include "tao/pegtl/parse.hpp"
 
+namespace peg = tao::pegtl;
+
 TEST_CASE("base_grammar_int_num") {
-  namespace peg = tao::pegtl;
-  peg::standard_trace<CAEParser::unsign_int_num>(peg::memory_input("1.", ""));
   CHECK_FALSE(
       peg::parse<CAEParser::unsign_int_num>(peg::memory_input("1.", "")));
+  CHECK_FALSE(
+      peg::parse<CAEParser::unsign_int_num>(peg::memory_input(".4", "")));
+  CHECK_FALSE(
+      peg::parse<CAEParser::unsign_int_num>(peg::memory_input("4e1", "")));
+  CHECK_FALSE(
+      peg::parse<CAEParser::unsign_int_num>(peg::memory_input("5.6", "")));
+  CHECK_FALSE(
+      peg::parse<CAEParser::unsign_int_num>(peg::memory_input("34er", "")));
+
+  CHECK(peg::parse<CAEParser::unsign_int_num>(peg::memory_input("34", "")));
+  CHECK(peg::parse<CAEParser::sign_int_num>(peg::memory_input("34", "")));
+  CHECK(peg::parse<CAEParser::sign_int_num>(peg::memory_input("-34", "")));
+}
+
+TEST_CASE("base_grammar_float_num") {
+  CHECK_FALSE(peg::parse<CAEParser::float_num>(peg::memory_input("1", "")));
+  CHECK(peg::parse<CAEParser::float_num>(peg::memory_input(".4", "")));
+  CHECK(peg::parse<CAEParser::float_num>(peg::memory_input("4e-1", "")));
+  CHECK_FALSE(
+      peg::parse<CAEParser::float_num>(peg::memory_input("4e-1.2", "")));
+  CHECK(peg::parse<CAEParser::float_num>(peg::memory_input("+5.6", "")));
+  CHECK(peg::parse<CAEParser::float_num>(peg::memory_input("5.", "")));
+  CHECK_FALSE(peg::parse<CAEParser::float_num>(peg::memory_input("34er", "")));
+  CHECK_FALSE(peg::parse<CAEParser::float_num>(peg::memory_input("34", "")));
 }
